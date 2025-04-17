dd550a0cac707f7a72c830b62ad0d923335ccebf
EgyetemPC <Egyetem@DESKTOP-8S29QAQ>
2025-04-17 14:15:49 +0200

    Added Javascript to FETCH API (GetAllUsers). Can now list all of the users.

---
+++ wwwroot/js/users.js
@@ -0,0 +1,29 @@
+﻿const searchForm = document.getElementById('searchForm');
+const searchBtn = document.getElementById('searchBtn');
+
+function listAllUsers() {
+    const tableData = document.getElementById('listUserData');
+
+    fetch('/api/UsersApi')
+        .then(result => result.json())
+        .then(users => {
+            users.forEach(user => {
+                tableData.innerHTML +=
+                    `<tr>
+                    <td>${user.id}</td>
+                    <td>${user.name}</td>
+                    <td>${user.email}</td>
+                    <td>${user.birthDate}</td>
+                    <td>${user.registrationDate}</td>
+                </tr>`
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+document.addEventListener('DOMContentLoaded', event => {
+    event.preventDefault();
+    listAllUsers();
+});
